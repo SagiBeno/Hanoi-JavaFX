8db0db4eb4043fd0b539186ad8a0745a18ba09c1
SagiBeno <benosagi321@gmail.com>
2025-12-27 09:20:32 +0100

    Modify width and height

--- src/main/java/org/example/hanoi/HanoiApp.java
+++ src/main/java/org/example/hanoi/HanoiApp.java
@@ -11,7 +11,7 @@ public class HanoiApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HanoiApp.class.getResource("hanoi-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 300);
         stage.setTitle("Hanoi");
         stage.setScene(scene);
         stage.show();
