724bb135cada2e5ca97467332dba05e25434c954
SagiBeno <benosagi321@gmail.com>
2025-12-27 09:18:43 +0100

    Copying hanoi-view, renaming files

--- src/main/java/org/example/hanoi/HanoiApp.java
+++ src/main/java/org/example/hanoi/HanoiApp.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HanoiApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HanoiApp.class.getResource("hanoi-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hanoi");
         stage.setScene(scene);
         stage.show();
     }
